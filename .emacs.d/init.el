a836d16cc53bd0a77434b23d11be57e8774964d1
An Huynh <zanton.zzz@gmail.com>
2019-12-31 22:57:45 +0900

    Rename inits -> init.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,36 +1,36 @@
 ;; Basic files & directories
 (setq user-emacs-dir "~/.emacs.d")
 (add-to-list 'load-path (concat user-emacs-dir "/lisp"))
-(setq inits-dir (concat user-emacs-dir "/inits"))
+(setq init-dir (concat user-emacs-dir "/init.d"))
 (setq custom-file (concat user-emacs-dir "/custom.el"))
 
 ;; Basic key bindings & options
-(load-file (concat inits-dir "/basics.el"))
-(load-file (concat inits-dir "/misc.el"))
+(load-file (concat init-dir "/basics.el"))
+(load-file (concat init-dir "/misc.el"))
 
 ;; Language modes
-(load-file (concat inits-dir "/c_mode.el"))
-(load-file (concat inits-dir "/markdown_mode.el"))
-(load-file (concat inits-dir "/cuda_mode.el"))
-(load-file (concat inits-dir "/cmake_mode.el"))
+(load-file (concat init-dir "/c_mode.el"))
+(load-file (concat init-dir "/markdown_mode.el"))
+(load-file (concat init-dir "/cuda_mode.el"))
+(load-file (concat init-dir "/cmake_mode.el"))
 
 ;; Highlight
-(load-file (concat inits-dir "/highlight_symbol.el"))
-(load-file (concat inits-dir "/breadcrumb.el"))
-(load-file (concat inits-dir "/symbol_overlay.el"))
+(load-file (concat init-dir "/highlight_symbol.el"))
+(load-file (concat init-dir "/breadcrumb.el"))
+(load-file (concat init-dir "/symbol_overlay.el"))
 
 ;; Copy
-(load-file (concat inits-dir "/thing_edit.el"))
+(load-file (concat init-dir "/thing_edit.el"))
 
 ;; Auto-Complete
-(load-file (concat inits-dir "/company_mode.el"))
-;(load-file (concat inits-dir "/auto_complete.el"))
+(load-file (concat init-dir "/company_mode.el"))
+;(load-file (concat init-dir "/auto_complete.el"))
 
 ;; Magit
-(load-file (concat inits-dir "/magit.el"))
+(load-file (concat init-dir "/magit.el"))
 
 ;; Helm
-(load-file (concat inits-dir "/helm.el"))
+(load-file (concat init-dir "/helm.el"))
 
 ;;(package-initialize)
 
