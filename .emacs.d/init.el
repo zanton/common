09f1d4595f80927ba0504a2cb9aaa679d6d3d555
An Huynh <zanton.zzz@gmail.com>
2022-06-27 16:25:33 +0900

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,27 +10,33 @@
 
 ;; Language modes
 (load-file (concat init-dir "/c_mode.el"))
-(load-file (concat init-dir "/markdown_mode.el"))
+;;(load-file (concat init-dir "/markdown_mode.el"))
 (load-file (concat init-dir "/cuda_mode.el"))
 (load-file (concat init-dir "/cmake_mode.el"))
+;;(load-file (concat init-dir "/lua_mode.el"))
 
 ;; Highlight
 (load-file (concat init-dir "/highlight_symbol.el"))
-(load-file (concat init-dir "/breadcrumb.el"))
+;;(load-file (concat init-dir "/breadcrumb.el")) ;; similar to bm.el
 (load-file (concat init-dir "/symbol_overlay.el"))
+(load-file (concat init-dir "/volatile_highlights.el"))
+(load-file (concat init-dir "/bm.el"))
 
-;; Copy
-(load-file (concat init-dir "/thing_edit.el"))
-
-;; Auto-Complete
+;; Completion
 (load-file (concat init-dir "/company_mode.el"))
-;(load-file (concat init-dir "/auto_complete.el"))
+;;(load-file (concat init-dir "/auto_complete.el"))
 
 ;; Magit
 (load-file (concat init-dir "/magit.el"))
 
 ;; Helm
 (load-file (concat init-dir "/helm.el"))
+(load-file (concat init-dir "/helm_swoop.el"))
+(load-file (concat init-dir "/helm_gtags.el"))
+
+;; Utilities
+(load-file (concat init-dir "/thing_edit.el"))
+(load-file (concat init-dir "/quickrun.el"))
 
 ;;(package-initialize)
 
